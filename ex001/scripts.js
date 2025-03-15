c25f8d0dbd6116adda4d97886267f718bd7a83ed
orodolfoso <orodolfoso.dev@gmail.com>
2025-03-15 18:16:25 -0300

    Estrutura de dados if

---
+++ ex001/scripts.js
@@ -0,0 +1,43 @@
+
+
+// 1 - FUNÇÃO PROMPT
+
+// const age = prompt('Digite a sua idade: ')
+
+// console.log(`Você tem ${age} anos`)
+
+// ________________________________________________________
+
+// 2 - FUNÇÃO ALERT
+
+// alert ('Olá usuário, clique no botão "OK" para proseguir com o site!')
+
+// ________________________________________________________
+
+// 3 - FUNÇÕES DO OBJETO MATH
+
+// console.log(Math.max(10 , 20 , 50))
+
+// console.log(Math.min(10 , 20 , 50))
+
+// console.log(Math.floor(5.14))
+
+// console.log(Math.ceil(5.14))
+
+// _________________________________________________________
+
+// 4 - OBJETO CONSOLE
+
+// console.log('teste')
+
+// console.error('error!')
+
+// console.warn('Aviso!')
+
+// 5 - ESTRUTURA DE CONTROLES - if
+
+const user = 'Rodolfo'
+
+if(user === 'Rodolfo' ) {
+    console.log("Acessado!")
+}
