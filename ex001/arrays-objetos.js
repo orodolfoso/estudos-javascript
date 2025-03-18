50ef46865f87dc517c1e4877b04f3d134f1b49b5
orodolfoso <orodolfoso.dev@gmail.com>
2025-03-18 10:25:30 -0300

    Object Literals

---
+++ ex001/arrays-objetos.js
@@ -0,0 +1,61 @@
+//1 - Arrays
+
+
+// const user = ['Rodolfo', 39, true, 70.2, []]
+
+// console.log(user)
+// console.log(typeof(user))
+
+
+
+// Mais sobre Arrays
+
+
+// const arr = ['a', 'b', 'c', 'd' ,'d']
+
+// console.log(arr[4])
+
+// console.log(arr[52])
+
+
+
+// 3 - Propriedades
+
+// const numbers = [1, 2 , 3 ,4 , 5]
+
+// console.log(numbers.length)
+// console.log(numbers['length'])
+
+// const myName = 'Rodolfo'
+
+// console.log(myName.length)
+
+// console.log(myName['length'])
+
+// 4 - Métodos
+
+// const  otherNumbers = [1 , 2 , 3]
+
+// const allNumbers = numbers.concat(otherNumbers)
+
+// console.log(allNumbers)
+
+// const text = 'algum texto'
+
+// console.log(text.toUpperCase())
+// console.log(typeof text.toUpperCase)
+// console.log(text.indexOf('g'))
+
+
+// 5 - Objetos (Obejct Literals)
+
+const person = {
+    name: 'Rodolfo',
+    age: 39,
+    profession: 'Programador'
+}
+
+console.log(person)
+console.log(person.name)
+console.log(person.name.length)
+console.log(typeof person)
