394c43b0c2d4d6b174eabd6f6c0e79ae28f7242c
orodolfoso <orodolfoso.dev@gmail.com>
2025-03-19 19:00:15 -0300

    Arrays e Objetos

--- ex001/arrays-objetos.js
+++ ex001/arrays-objetos.js
@@ -1,4 +1,4 @@
-//1 - Arrays
+// 1 - Arrays
 
 
 // const user = ['Rodolfo', 39, true, 70.2, []]
@@ -8,7 +8,7 @@
 
 
 
-// Mais sobre Arrays
+// 2 - Mais sobre Arrays
 
 
 // const arr = ['a', 'b', 'c', 'd' ,'d']
@@ -49,13 +49,85 @@
 
 // 5 - Objetos (Obejct Literals)
 
-const person = {
-    name: 'Rodolfo',
-    age: 39,
-    profession: 'Programador'
-}
+// const person = {
+//     name: 'Rodolfo',
+//     age: 39,
+//     profession: 'Programador'
+// }
 
-console.log(person)
-console.log(person.name)
-console.log(person.name.length)
-console.log(typeof person)
+// console.log(person)
+// console.log(person.name)
+// console.log(person.name.length)
+// console.log(typeof person)
+
+// 6 - Criando e deletando propriedades
+
+// const car = {
+//     engine: 2.0,
+//     brand: 'VW',
+//     model: 'Tiguan',
+//     km: 20000,
+
+// }
+
+// console.log(car)
+// car.doors = 4
+// console.log(car)
+// delete car.km
+// console.log(car)
+
+// 7 - Mais sobre objetos
+
+// const obj = {
+//     a: 'teste',
+//     b: true
+// }
+
+// console.log(obj instanceof Object)
+
+// const obj2 = {
+//     c: []
+// }
+
+// Object.assign(obj2, obj)
+// console.log(obj2)
+
+// 8 - Conhecendo melhor o objeto
+
+// console.log(Object.keys(obj))
+// console.log(Object.keys(obj2))
+// console.log(Object.keys(car))
+// console.log(Object.entries(car))
+
+// 9 - Mutação (Mutability)
+
+// const a = {
+//     name:  'Rodolfo'
+// }
+
+// const b = a
+
+// console.log(a)
+// console.log(b)
+// console.log(a === b)
+
+// a.age = 39
+
+// console.log(a)
+// console.log(b)
+
+// delete b.age
+
+// console.log(a)
+// console.log(b)
+
+// 10 - Loops em arrays
+
+// const users = ['Rodolfo', 'Lucas', 'Renata', 'Maria', 'José']
+
+// for(let i = 0; i < users.length; i++) {
+//     console.log(`Listando usuário: ${users[i]}`)
+// }
+
+// 11 - Push e PoP
+
